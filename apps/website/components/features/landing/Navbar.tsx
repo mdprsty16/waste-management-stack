0e2bb7456c6d2f9578c003a1867ef9ab25dee5c7
abansybana09 <abansybana322@gmail.com>
2026-06-16 16:25:22 +0700

    hehehhe salah  format

--- apps/website/components/features/landing/Navbar.tsx
+++ apps/website/components/features/landing/Navbar.tsx
@@ -50,7 +50,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between">
           <a href="#beranda" className="flex items-center gap-3 group">
             <Image
-              src="/logo2.png"
+              src="/logo2.webp"
               alt="Logo"
               width={44}
               height={44}
